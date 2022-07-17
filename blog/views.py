d876cfda7088a6c465210aad5856999714680325
Maxim Pochinskiy <mvp280102@gmail.com>
2022-07-17 18:01:01 +0500

    Add post detail view.

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Post
 
 
 def index(request):
     post_list = Post.objects.order_by('-pub_date')
-    context = {'post_list': post_list}
-    return render(request, 'blog/index.html', context)
+    return render(request, 'blog/index.html', {'post_list': post_list})
+
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    return render(request, 'blog/detail.html', {'post': post})
