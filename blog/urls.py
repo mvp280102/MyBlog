f1345f92723758f2c86c3cd12669d7492b7c0cd4
Maxim Pochinskiy <mvp280102@gmail.com>
2022-07-17 23:38:09 +0500

    Add post route to urls.

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,5 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('<int:pk>/', views.PostDetailView.as_view(), name='detail'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='detail'),
 ]
