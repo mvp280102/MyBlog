e476a4969c90899b77ae41f9a981733bc2a159a6
Maxim Pochinskiy <mvp280102@gmail.com>
2022-07-17 18:00:36 +0500

    Add post detail url.

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 
+app_name = 'blog'
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:post_id>/', views.post_detail, name='detail'),
 ]
