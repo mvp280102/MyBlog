baff63706aba227db1ecd2905441e057f775d80d
Maxim Pochinskiy <mvp280102@gmail.com>
2022-07-17 16:22:37 +0500

    Create Author, Post and Comment models.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=64)
+    surname = models.CharField(max_length=64)
+    reg_date = models.DateField(default=timezone.now)
+
+
+class Post(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    title = models.CharField(max_length=256)
+    text = models.TextField()
+    pub_date = models.DateField(default=timezone.now)
+    likes = models.IntegerField(default=0)
+    dislikes = models.IntegerField(default=0)
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    text = models.TextField()
+    pub_date = models.DateField(default=timezone.now)
