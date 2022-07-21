5ab788c9650b60fe26cf5ff234ebe7414824e024
Maxim Pochinskiy <mvp280102@gmail.com>
2022-07-21 11:45:57 +0500

    Add archive view.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, CreateView, UpdateView, DetailView
 from .models import Post
 
 
@@ -7,7 +7,27 @@ class IndexView(ListView):
     context_object_name = 'post_list'
 
     def get_queryset(self):
+        return Post.objects.order_by('-pub_date')[:5]
+
+
+class ArchiveView(ListView):
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
+
+    def get_queryset(self):
         return Post.objects.order_by('-pub_date')
+
+
+class PostCreateView(CreateView):
+    model = Post
+    template_name = 'blog/create.html'
+    fields = ['author', 'title', 'text']
+
+
+class PostEditView(UpdateView):
+    model = Post
+    template_name = 'blog/edit.html'
+    fields = ['title', 'text']
 
 
 class PostDetailView(DetailView):
