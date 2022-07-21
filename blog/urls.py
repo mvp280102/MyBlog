5d6a431cecf4e659eb3ef707ce4cae56b73006f3
Maxim Pochinskiy <mvp280102@gmail.com>
2022-07-21 11:45:40 +0500

    Add blog archive url.

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from . import views
+from . import views, utils
 
 
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
-    path('post/<int:pk>/', views.PostDetailView.as_view(), name='detail'),
+    path('', views.IndexView.as_view(extra_context=utils.index_context), name='index'),
+    path('archive/', views.ArchiveView.as_view(extra_context=utils.archive_context), name='archive'),
+    path('create/', views.PostCreateView.as_view(), name='post_create'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
+    path('post/<int:pk>/edit', views.PostEditView.as_view(), name='post_edit'),
 ]
